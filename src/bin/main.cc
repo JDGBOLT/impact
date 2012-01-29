4e6058cd3e46a9d486239c3d985d45cd4718ff0c
Joshua Gwinn <jdgbolt@gmail.com>
2012-01-28 17:35:46 -0700

    Added basic cmake build system and main function which compiles and runs

---
+++ src/bin/main.cc
@@ -0,0 +1,12 @@
+/**
+ * @file /src/bin/main.cc
+ * @brief The main function for the game impact, containing the main loop
+ * @author Joshua Gwinn (jdgbolt@gmail.com)
+ * @date 2012-01-28
+ */
+
+int
+main(int argc, const char *argv[])
+{
+   return 0;
+}
