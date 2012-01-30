9a523c3011af03c9dd9e5e769900b8df27f509b4
Joshua Gwinn <jdgbolt@gmail.com>
2012-01-30 04:24:36 -0700

    Add skeleton input play state module.
    This file doesn't do much, other than to inherit from the InputState class, it
    is merely here to make any changes needing to be done to the input module at a
    later date easier, and also to make the names for each state module be uniform.

---
+++ src/lib/input/input_state_play.hh
@@ -0,0 +1,18 @@
+/**
+ * @file input_state_play.hh
+ * @brief The header file for the InputPlay class, which is the input module of
+ * the Play state, which is run during normal gameplay
+ * @author Joshua Gwinn (jdgbolt@gmail.com)
+ * @version 
+ * @date 2012-01-30
+ */
+
+#ifndef _INPUT_STATE_PLAY_HH_
+#define _INPUT_STATE_PLAY_HH_
+
+#include "input_state.hh"
+class InputPlay : public InputState
+{
+};
+
+#endif
